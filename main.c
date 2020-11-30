d9d6013649fc26b3f9fffd9f5e03fd38011eda61
Ohanna Dezidério <ohannadeziderio@gmail.com>
2020-11-30 03:43:12 -0300

    Calculando b e k
    - Calculou-se b e k
    - Modificou-se o nome do arquivo dados para input

--- main.c
+++ main.c
@@ -4,46 +4,38 @@
 #include "methods.h"
 
 int main() {
-    FILE *dados;
+    FILE *input;
 
     // Alocando dinamicamente as variáveis
-    int *S0 = alocar_din_int(2);
-    int *I0 = alocar_din_int(1);
-    int *R0 = alocar_din_int(1);
-    double *h = alocar_din_double(2);
-    int *N_b = alocar_din_int(2);
-    int *T_b = alocar_din_int(2);
-    int *S_b0 = alocar_din_int(2);
-    int *l_b0 = alocar_din_int(2);
-    int *m_k = alocar_din_int(1);
-    int *n_k = alocar_din_int(2);
-    int *T_k = alocar_din_int(2);
+    double *S0 = alocar_din(2);
+    double *I0 = alocar_din(1);
+    double *R0 = alocar_din(1);
+    double *h = alocar_din(2);
+    double *N_b = alocar_din(2);
+    double *T_b = alocar_din(2);
+    double *S_b0 = alocar_din(2);
+    double *I_b0 = alocar_din(2);
+    double *m_k = alocar_din(1);
+    double *n_k = alocar_din(2);
+    double *T_k = alocar_din(2);
 
     // Abertura do arquivo de especificação
-    dados = fopen("dados.txt", "r"); 
+    input = fopen("input.txt", "r"); 
 
     // Verifica se o arquivo foi aberto
-    if (dados == NULL) {
+    if (input == NULL) {
         printf("ERRO! O arquivo não foi aberto!\n");
 
-        return 0;
+        exit(1);
     }
 
-    fscanf(dados,"%i,%i,%i,%lf,%i,%i,%i,%i,%i,%i,%i\n", S0, I0, R0, h, N_b, T_b, S_b0, l_b0, m_k, n_k, T_k);
+    fscanf(input,"%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf,%lf\n", S0, I0, R0, h, N_b, T_b, S_b0, I_b0, m_k, n_k, T_k);
 
-    printf("S0: %i \n", *S0);
-    printf("I0: %i \n", *I0);
-    printf("R0: %i \n", *R0);
-    printf("h: %.2lf \n", *h);
-    printf("N_b: %i \n", *N_b);
-    printf("T_b: %i \n", *T_b);
-    printf("Sb0: %i \n", *S_b0);
-    printf("l_b0: %i \n", *l_b0);
-    printf("m_k: %i \n", *m_k);
-    printf("n_k: %i \n", *n_k);
-    printf("T_k: %i \n", *T_k);
+    //Calculando as constantes
+    double b = (*N_b)/((*T_b) * (*S_b0) * (*I_b0));
+    double k = (*m_k)/((*n_k) * (*T_k));
 
-    fclose(dados);
+    fclose(input);
 
     return 0;
 }
